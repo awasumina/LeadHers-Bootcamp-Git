2a816f087505b1e1d37e8bf93db2a6e54ffee894
sumina <suminaawa123@gmail.com>
2024-10-17 16:14:59 +0545

    remote instruction added

--- index.js
+++ index.js
@@ -3,9 +3,6 @@
 // git init - ek choti kei kam garesi feri NavigationPreloadManager, if reinitailized sabai link break hunxa existing
 // git add filename - to individual file
 // git add .        - to add all files
-
-
-
-
-
-
+// git remote -v     - to look the link that can be made betn local and reportError, i.e. view remote origin
+// git branch       - to look the branch main or master
+//  -u means untracked files, can be written as --set-upstream
